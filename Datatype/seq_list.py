f662e46140fcbfbbe6fbf3af3bc007acba87ae2c
Soumyajitsantra8335 <sunnysantra64@gmail.com>
2026-06-10 15:02:51 +0530

    s

--- Datatype/seq_list.py
+++ Datatype/seq_list.py
@@ -2,3 +2,11 @@ student = ["rahul","tamal","shyam","ranjan",5,6]
 
 print("List = ",student)
 print("datatype = ",type(student))
+
+#append
+student.append(8)
+print("append List = ",student)
+
+#Insert
+student.insert(1,"subash")
+print("Insert List = ",student)
---
+++ Datatype/seq_tuple.py
@@ -0,0 +1,9 @@
+tuplee = ("rana","tamal",54,6,8,78)
+
+print(tuplee)
+print("datatype = ",type(tuplee))
+
+#add Krishna
+#, is mandatory
+tuplee1=tuplee + ("Krishna",)
+print(tuplee1)
