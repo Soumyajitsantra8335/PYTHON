28715dc05e46612ef97907036234d3dbf8ffc3e7
Soumyajitsantra8335 <sunnysantra64@gmail.com>
2026-06-10 15:33:11 +0530

    ss

---
+++ Datatype/set.py
@@ -0,0 +1,22 @@
+y = {1,2,3,6,4,9,8,7,9,7,5,6,4,56,6,8,4}
+
+print("Set = ",y)
+print("Datatype = ",type(y))
+
+#Duplicate remove
+y = {1,2,3,6,4,9,8,7,9,7,5,6,4,56,6,8,4,4}
+print("Duplicate elements =",y)
+
+#adding elements
+y.add(110)
+print("adding elements = ",y)
+
+#update elements
+y.update([120,145])
+print("Update elements =",y)
+
+#remove elements
+y.remove(120)
+print("Remove elements =",y)
+
+#
---
+++ Datatype/set_forzen.py
@@ -0,0 +1,8 @@
+#immuatable
+num = frozenset([2,6,9,8,9,9,8,])
+print ("frozenset = ",num)
+print("Datatype = ", type(num))
+
+#adding value no
+num.add(40)
+print(num)
