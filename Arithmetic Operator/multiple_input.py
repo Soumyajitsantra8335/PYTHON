4bf5b526772988674fb1fffa0c01013c951ceacd
Soumyajitsantra8335 <sunnysantra64@gmail.com>
2026-06-12 09:53:34 +0530

    s

---
+++ Arithmetic Operator/multiple_input.py
@@ -0,0 +1,6 @@
+x = int(input("Enter Your First Number = "))
+y= int(input("Enter Your second number = "))
+
+Mul = x * y
+
+print("Multipilacation = ", Mul)
---
+++ Arithmetic Operator/mutiple.py
@@ -0,0 +1,9 @@
+a = 5
+b = 5
+
+Mul = a * b
+
+print("First Number = ",a)
+print("Second Number = ",b)
+print("Multipilacation = ",Mul)
+print("Datatype = ",type(Mul))
